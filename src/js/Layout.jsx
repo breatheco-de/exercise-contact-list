d22d1cd5b451bf823b62b7032ccd02bb5809a7bb
Deimian Vásquez <deimianvasquez@gmail.com>
2024-03-22 14:00:56 -0400

    delete library flux

--- src/js/Layout.jsx
+++ src/js/Layout.jsx
@@ -1,26 +1,47 @@
 import React from 'react';
-import Flux from "@4geeksacademy/react-flux-dash";
+// import Flux from "@4geeksacademy/react-flux-dash";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import Contacts from "./views/Contacts.jsx";
 import AddContact from "./views/AddContact.jsx";
 
-export default class Layout extends Flux.View {
-    render() {
-        return (
-            <div>
-                <BrowserRouter>
-                    <div>
-                        <Switch>
-                            <Route exact path="/index.html" component={Contacts} />
-                            <Route exact path="/" component={Contacts} />
-                            <Route exact path="/contacts" component={Contacts} />
-                            <Route exact path="/add" component={AddContact} />
-                            <Route exact path="/edit" component={AddContact} />
-                            <Route render={() => <h1 className="notfound">Not found!</h1>} />
-                        </Switch>
-                    </div>
-                </BrowserRouter>
-            </div>
-        );
-    }
+// export default class Layout extends Flux.View {
+//     render() {
+//         return (
+//             <div>
+//                 <BrowserRouter>
+//                     <div>
+//                         <Switch>
+//                             <Route exact path="/index.html" component={Contacts} />
+//                             <Route exact path="/" component={Contacts} />
+//                             <Route exact path="/contacts" component={Contacts} />
+//                             <Route exact path="/add" component={AddContact} />
+//                             <Route exact path="/edit" component={AddContact} />
+//                             <Route render={() => <h1 className="notfound">Not found!</h1>} />
+//                         </Switch>
+//                     </div>
+//                 </BrowserRouter>
+//             </div>
+//         );
+//     }
+// }
+
+function Layout() {
+    return (
+        <div>
+            <BrowserRouter>
+                <div>
+                    <Switch>
+                        <Route exact path="/index.html" component={Contacts} />
+                        <Route exact path="/" component={Contacts} />
+                        <Route exact path="/contacts" component={Contacts} />
+                        <Route exact path="/add" component={AddContact} />
+                        <Route exact path="/edit" component={AddContact} />
+                        <Route render={() => <h1 className="notfound">Not found!</h1>} />
+                    </Switch>
+                </div>
+            </BrowserRouter>
+        </div>
+    );
 }
+
+export default Layout;
